f611fbebb35853001e15c0eb542f920396b68d5a
Matt Carrick <carrickm@dca904961218.ant.amazon.com>
2017-09-13 10:59:52 +0100

    update replace again

--- serve.py
+++ serve.py
@@ -47,7 +47,7 @@ class StringGenerator(object):
             elif resp.json()['response']['players'][0]['personastate'] == 1 or resp.json()['response']['players'][0]['personastate'] == 5 or resp.json()['response']['players'][0]['personastate'] == 6:
                 game = "Online"       
 
-        c.execute("UPDATE games set game = REPLACE('Away','Away','Offline')")
+        c.execute("UPDATE games set game = REPLACE(game,'Away','Offline') WHERE game LIKE 'Away'")
         c.execute("INSERT INTO games VALUES ('"+date+"','"+game+"')")
         conn.commit()
         conn.close()
